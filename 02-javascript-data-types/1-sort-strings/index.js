c5a0dad742b18efa6515b38dea8fd12e12edca6d
gregory.usenko <hitman.ezak@gmail.com>
2023-09-19 17:03:51 +0300

    add sort array function

--- 02-javascript-data-types/1-sort-strings/index.js
+++ 02-javascript-data-types/1-sort-strings/index.js
@@ -4,6 +4,20 @@
  * @param {string} [param="asc"] param - the sorting type "asc" or "desc"
  * @returns {string[]}
  */
-export function sortStrings(arr, param = 'asc') {
-
+export function sortStrings(arr, param = "asc") {
+  const copyArr = [...arr];
+  switch (param) {
+    case "desc":
+      return sortArr(copyArr, -1);
+    case "asc":
+    default:
+      return sortArr(copyArr, 1);
+  }
+  function sortArr(array, direction) {
+    return array.sort(
+      (str1, str2) =>
+        direction *
+        str1.localeCompare(str2, ["ru", "en"], { caseFirst: "upper" })
+    );
+  }
 }
