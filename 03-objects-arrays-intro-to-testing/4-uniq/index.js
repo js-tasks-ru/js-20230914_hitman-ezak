55ad1e451c2bf8a03b7f4a83aeeeaca38fb27d3a
gregory.usenko <hitman.ezak@gmail.com>
2023-09-23 10:48:26 +0300

    add a uniq function

--- 03-objects-arrays-intro-to-testing/4-uniq/index.js
+++ 03-objects-arrays-intro-to-testing/4-uniq/index.js
@@ -3,6 +3,10 @@
  * @param {*[]} arr - the array of primitive values
  * @returns {*[]} - the new array with uniq values
  */
-export function uniq(arr) {
-
+export function uniq(arr = []) {
+  const myMap = new Map();
+  arr.forEach((value) => {
+    myMap.set(value, value);
+  });
+  return Array.from(myMap.values());
 }
