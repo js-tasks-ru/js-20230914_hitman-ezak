3b5bb4388abba983c06dd00a77525d01aac4d084
gregory.usenko <hitman.ezak@gmail.com>
2023-09-25 18:12:22 +0300

    add function trimSymbols

--- 03-objects-arrays-intro-to-testing/3-trim-symbols/index.js
+++ 03-objects-arrays-intro-to-testing/3-trim-symbols/index.js
@@ -5,5 +5,18 @@
  * @returns {string} - the new string without extra symbols according passed size
  */
 export function trimSymbols(string, size) {
+  if (size === 0) {
+    return "";
+  } else if (size === undefined) {
+    return string;
+  }
 
+  const firstSlice = string.slice(0, size);
+  const restString = [...string.slice(size)];
+  return restString.reduce((accumString, char) => {
+    if (!accumString.endsWith(char.repeat(size))) {
+      accumString += char;
+    }
+    return accumString;
+  }, firstSlice);
 }
