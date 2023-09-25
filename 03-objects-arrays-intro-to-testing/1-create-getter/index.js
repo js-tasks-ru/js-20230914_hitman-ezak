b74748a6f65106d7baefe49876806b74a561796f
gregory.usenko <hitman.ezak@gmail.com>
2023-09-25 11:57:03 +0300

    add function createGetter

--- 03-objects-arrays-intro-to-testing/1-create-getter/index.js
+++ 03-objects-arrays-intro-to-testing/1-create-getter/index.js
@@ -4,5 +4,16 @@
  * @returns {function} - function-getter which allow get value from object by set path
  */
 export function createGetter(path) {
-
+  const pathArray = path.split(".");
+  return (obj) => {
+    let result = obj;
+    const getValue = (arr) => {
+      if (!arr.length || !result) {
+        return result;
+      }
+      result = result[arr.shift()];
+      return getValue(arr);
+    };
+    return getValue([...pathArray]);
+  };
 }
