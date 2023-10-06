9c8e40c992fd70b4f95f43d092d74c3cfafbf2f5
gregory.usenko <hitman.ezak@gmail.com>
2023-10-06 13:16:15 +0300

    feat: add notification, sortable table v1

--- 05-dom-document-loading/1-notification/index.js
+++ 05-dom-document-loading/1-notification/index.js
@@ -1,3 +1,58 @@
 export default class NotificationMessage {
+  static activeNotification;
+  constructor(message, { duration = 2000, type = "" } = {}) {
+    this.message = message;
+    this.duration = duration;
+    this.type = type;
+    this.render();
+  }
 
+  render() {
+    const { template } = this;
+
+    const element = document.createElement("div");
+    element.innerHTML = template;
+
+    this.element = element.firstElementChild;
+    NotificationMessage.activeNotification = this.element;
+  }
+
+  get template() {
+    const { message, type, duration } = this;
+    const durationInSeconds = duration / 1000 + "s";
+    return `
+    <div class="notification ${type}" style="--value:${durationInSeconds}">
+        <div class="timer"></div>
+        <div class="inner-wrapper">
+            <div class="notification-header">success</div>
+            <div class="notification-body">
+                ${message}
+            </div>
+        </div>
+    </div>`;
+  }
+
+  show(parent = document.body) {
+    const { element, duration } = this;
+
+    parent.append(element);
+    this.hide();
+  }
+
+  hide = () => {
+    setTimeout(() => {
+      this.remove();
+    }, this.duration);
+  };
+
+  remove = () => {
+    this.element.remove();
+  };
+
+  destroy() {
+    clearTimeout(this.hide);
+    this.remove;
+    this.element = null;
+    NotificationMessage.activeNotification = null;
+  }
 }
